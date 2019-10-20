423a2ee2030a5e0bb5f90cad76a8db6861c1aac5
abegilerova <abegilerova@gmail.com>
2019-10-20 15:38:33 -0400

    searching and displaying countries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchCountry from './components/SearchCountry';
+import SearchResult from './components/SearchResult';
 
 const App = () => {
-
 
   const [countries, setCountries] = useState([]);
   const [countrySearch, setCountrySearch] = useState('');
   const [foundCountry, setFoundCountry] = useState('');
-
-
 
   const all = 'https://restcountries.eu/rest/v2/all';
   useEffect(() => {
@@ -21,38 +19,17 @@ const App = () => {
         setCountries(response.data)
       })
   }, [])
-  // console.log('render', countries.length, 'countries');
-
-
-
-  // const displayCountryNames = () => countryNames.map(countryName => <li>{countryName}</li>);
 
   const handleCountrySearch = (event) => {
     setCountrySearch(event.target.value);
-    console.log("country search", countrySearch);
-    const countryNames = countries.map(country => country.name);
-    const found = countryNames.filter(countryName => countryName.includes(countrySearch));
-    setFoundCountry(found);
-
   }
 
   console.log("foundCountry", foundCountry);
 
-  const displayFoundCountry = () => foundCountry.length > 0 ? <li>{foundCountry}</li> : <li>no search</li>;
-
-
-
-
-
-
-
-
   return (
     <div>
       <SearchCountry countrySearch={countrySearch} handleCountrySearch={handleCountrySearch} />
-      <ul>
-        {displayFoundCountry()}
-      </ul>
+      <SearchResult countries={countries} countrySearch={countrySearch} setFoundCountry={setFoundCountry} foundCountry={foundCountry} />
     </div>
   );
 }
---
+++ src/components/SearchResult.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Country from './Country';
+
+
+const SearchResult = ({ countries, countrySearch, setCountrySearch }) => {
+
+    if (countrySearch === '') return null
+
+    const found = countries.filter(country => country.name.toLowerCase().includes(countrySearch.toLowerCase()));
+
+    if (found.length > 10) {
+        return <div>Too many matches</div>
+    } else if (found.length > 1) {
+        return (found.map(country => <div>{country.name}</div>))
+    } else if (found.length === 1) {
+        return (
+            <Country foundCountry={found[0]} />
+        )
+    }
+
+
+
+}
+
+export default SearchResult;
